f3727229edbfce9014b7df137e38bc436f4491f0
Varun Pawar <varunpwr897@gmail.com>
2018-06-27 17:27:24 +0530

    edge_feasible_mp improved

--- space_manip_map.m
+++ space_manip_map.m
@@ -7,10 +7,28 @@ l2 =50;
 if indx == -1%inverse map
     d =z_max - pos(3);
     alpha = acos(((l1^2 + l2^2) - pos(1)^2)/(2*l1*l2));
-    beta = acos((pos(1)^2 + l1^2 - l2^2)/(2*l1*pos(1)));
+    beta = acos((-l2^2 + pos(1)^2 + l1^2 )/(2*l1*pos(1)));
     %there are 2 possible solutions to theta(arm2) and phi(arm1)
     theta = [ pi + alpha, pi - alpha];
+    if theta(1) >= 2*pi
+        theta(1) = theta(1) - 2*pi;
+    elseif theta(1) < 0
+        theta(1) = theta(1) + 2*pi;
+    elseif theta(1) >= 2*pi
+        theta(2) = theta(2) - 2*pi;
+    elseif theta(2) < 0
+        theta(2) = theta(2) + 2*pi;
+    end
     phi = [pos(2) + beta, pos(2) - beta];
-    f = [d, theta, phi]; %pass all values 
+    if phi(1) >= 2*pi
+        phi(1) = phi(1) - 2*pi;
+    elseif theta(1) < 0
+        phi(1) = phi(1) + 2*pi;
+    elseif phi(1) >= 2*pi
+        phi(2) = phi(2) - 2*pi;
+    elseif phi(2) < 0
+        phi(2) = phi(2) + 2*pi;
+    end
+    f = [d, theta(1), phi(1);d, theta(2), phi(2)]; %pass all values 
 end
 end
